216e13dc9f6a9b4ec71064d66cd48897bce8acd2
yawkat <jonas.konrad@oracle.com>
2025-11-13 09:23:55 +0100

    split up old and new impl: new maven subproject

--- multipart-vintage/src/test/java/io/netty/contrib/multipart/vintage/HttpPostRequestEncoderTest.java
+++ multipart-vintage/src/test/java/io/netty/contrib/multipart/vintage/HttpPostRequestEncoderTest.java
@@ -15,8 +15,8 @@
  */
 package io.netty.contrib.multipart.vintage;
 
-import io.netty.contrib.handler.codec.http.multipart.HttpPostRequestEncoder.EncoderMode;
-import io.netty.contrib.handler.codec.http.multipart.HttpPostRequestEncoder.ErrorDataEncoderException;
+import io.netty.contrib.multipart.vintage.HttpPostRequestEncoder.EncoderMode;
+import io.netty.contrib.multipart.vintage.HttpPostRequestEncoder.ErrorDataEncoderException;
 import io.netty5.buffer.Buffer;
 import io.netty5.buffer.BufferAllocator;
 import io.netty5.buffer.DefaultBufferAllocators;
